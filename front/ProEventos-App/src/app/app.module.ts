ce238ce65f156a3d254449f7079b9d3a6ac025aa
Benê <alvespbene@gmail.com>
2023-11-21 15:18:30 -0300

    Aula 4

--- front/ProEventos-App/src/app/app.module.ts
+++ front/ProEventos-App/src/app/app.module.ts
@@ -1,20 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
-import { HttpClientModule } from '@angular/common/http';
+import { NavComponent } from './nav/nav.component';
+
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     EventosComponent,
+    NavComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    CollapseModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -13,11 +13,41 @@ export class EventosComponent implements OnInit {
   }
 
   constructor(private http: HttpClient) {}
-  public eventos: any;
+
+  public eventos: any = [];
+  public filterEventos: any = [];
+  private _searchList: string = ''
+
+  widthImg = 250;
+  marginImg = 2;
+  showImg = false;
+
+  showImage(){
+    this.showImg = !this.showImg;
+  }
+
+  public get search() : string{
+    return this._searchList;
+  }
+  public set search(value: string){
+    this._searchList = value;
+    this.filterEventos = this._searchList ? this.searchEventos(this._searchList) : this.eventos;
+  }
+
+  searchEventos(searchBy: string): any{
+    searchBy = searchBy.toLocaleLowerCase();
+    return this.eventos.filter(
+      (e: {tema: string; local:string}) =>
+       e.tema.toLocaleLowerCase().indexOf(searchBy) !== -1 ||
+       e.local.toLocaleLowerCase().indexOf(searchBy) !== -1)
+  }
 
   public getEventos(): void {
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response,
+        this.filterEventos = this.eventos
+      },
       error => console.log(error)
     )
   }
